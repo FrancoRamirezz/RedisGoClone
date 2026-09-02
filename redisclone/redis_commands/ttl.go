8db5c77de8451e90cc466eb1d577e3ad563bf020
Franco <96508706+FrancoRamirezz@users.noreply.github.com>
2026-09-01 22:08:12 -0700

    Update ttl.go

--- redisclone/redis_commands/ttl.go
+++ redisclone/redis_commands/ttl.go
@@ -1,8 +1,8 @@
 package rediscommands
 
 import (
-	"Pokemonscraper/redisclone/Datastructure"
-	"Pokemonscraper/redisclone/resp"
+	"redisclone/Datastructure"
+	"redisclone/resp"
 	"strconv"
 	"time"
 )
